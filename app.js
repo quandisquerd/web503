ecff27bb3ceedb4897e69e60863d7c999695f003
Le Trong Dat <datlt2306@gmail.com>
2022-03-08 11:21:53 +0700

    basic express

--- app.js
+++ app.js
@@ -1,29 +1,41 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    if(url === '/product'){
-        res.setHeader('Content-Type','application/json');
-        const data = [{id: 1, name: "Product A"}, {id: 2, name: "Product B"}];
-        res.end(JSON.stringify(data));
-    } else if(url === "/posts"){
-        res.setHeader('Content-Type','application/json');
-        const data = [{id: 1, name: "Post A"}, {id: 2, name: "Post B"}];
-        res.end(JSON.stringify(data));
+const express = require('express');
+const app = express();
+// middleware
+const check = (req, res, next) => {
+    const status = true;
+    if(status){
+        console.log("Chào sếp")
+        next();
     } else {
-        res.setHeader('Content-Type','text/html');
-        res.write("<html>");
-        res.write("<body>");
-            res.write("<h1>Home Page</h1>")
-        res.write("</body>");
-        res.write("</html>");
-        res.end();
+        console.log("Anh không có quyền truy cập");
     }
+}
+app.get('/api/products', check, (req, res) => {
+    const products = [
+        {id: 1, name: "Product A"},
+        {id: 2, name: "Product B"}
+    ];
+    res.json(products);
 });
 
+// const server = http.createServer((req, res) => {
+//     console.log('url', req.url);
+//     if(req.url === "/"){
+//         res.setHeader('Content-Type',"text/html");
+//         res.write("<html><body><h1>Home Page</h1></body></html>")
+//         res.end();
+//     } else if(req.url === "/api/products"){
+//         const products = [
+//             {id: 1, name: "Product A"},
+//             {id: 2, name: "Product B"}
+//         ];
+//         res.end(JSON.stringify(products));
+//     } else {
+//         console.log('Giờ thì em biết rồi');
+//     }
+// });
 
 const PORT = 3001;
-
-server.listen(PORT, () => {
-    console.log(`Server running port ${PORT}`);
+app.listen(PORT, () => {
+    console.log("Server is running port", PORT);
 })

70e471386dc2111f7ead951f108f1dcbd8fc6f30
Le Trong Dat <datlt2306@gmail.com>
2022-03-10 15:16:14 +0700

    Buổi 2

--- src/app.js
+++ src/app.js
@@ -1,45 +1,19 @@
-// const http = require('http');
-const express = require('express');
-const cors = require('cors');
+// const express = require('express');
+import express from 'express';
+import cors from 'cors';
+import productRoute from './routes/product';
+import morgan from 'morgan';
 
 const app = express();
+// middleware
 app.use(cors())
+app.use(morgan('tiny'))
+app.use(express.json())
 
-const checkAuth = (req, res,next) => {
-    const isAdmin = true;
-    if(isAdmin){
-        next();
-    } else {
-        console.log('Chim cút');
-    }
-}
-app.get('/', (req, res) => {
-    res.send("<h1>Home Page</h1>")
-})
-app.get('/api/products', checkAuth, (req, res) => {
-    const data = [
-        {id: 1, name: "Product A"}, 
-        {id: 2, name: "Product B"}
-    ];
-    res.json(data);
-});
-// const server = http.createServer((req, res) => {
-//     const url = req.url;
-//     console.log(url);
-//     if(url === "/api/products"){
-//         const data = [
-//             {id: 1, name: "Product A"}, 
-//             {id: 2, name: "Product B"}
-//         ];
-//         res.end(JSON.stringify(data))
-//     } else if( url === "/api/posts"){
-//         console.log("API Post")
-//     } else {
-//         res.setHeader("Content-Type","text/html");
-//         res.write("<html><body><h1>Home Page</h1></body></html>");
-//         res.end();
-//     }
-// })
+// routes
+app.use("/api", productRoute)
+
+// connect
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log("Server của bạn đang chạy cổng ", PORT);

9abfc2a308fcaf461a7d0cc952b8eed45125b5d2
Le Trong Dat <datlt2306@gmail.com>
2022-03-10 11:06:44 +0700

    Buổi 2 - update

--- routes/product.js
+++ routes/product.js
@@ -1,42 +1,23 @@
 import { Router } from 'express';
+import { checkAuth } from '../middlewares/checkAuth' 
 const router = Router();
+// fake data
+const products = [
+    {id: 1, name: "Product A"},
+    {id: 2, name: "Product B"}
+];
 
-const check = (req, res, next) => {
-    const status = true;
-    if(status){
-        next();
-    } else {
-        console.log("Anh không có quyền truy cập");
-    }
-}
-router.get('/products', check, (req, res) => {
-    const products = [
-        {id: 1, name: "Product A"},
-        {id: 2, name: "Product B"}
-    ];
+router.get('/products', checkAuth, (req, res) => {
     res.json(products);
 });
-router.get('/product/:id', check, (req, res) => {
-    const products = [
-        {id: 1, name: "Product A"},
-        {id: 2, name: "Product B"}
-    ];
+router.get('/product/:id', checkAuth, (req, res) => {
     res.json(products.find(item => item.id === +req.params.id));
 });
-router.post('/products', check, (req, res) => {
-    const products = [
-        {id: 1, name: "Product A"},
-        {id: 2, name: "Product B"}
-    ];
+router.post('/products', checkAuth, (req, res) => {
     products.push(req.body);
     res.json(products);
 });
-
-router.delete('/product/:id', check, (req, res) => {
-    const products = [
-        {id: 1, name: "Product A"},
-        {id: 2, name: "Product B"}
-    ];
+router.delete('/product/:id', checkAuth, (req, res) => {
     res.json(products.filter(item => item.id !== +req.params.id));
 });
 

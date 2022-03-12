347b334d3bb57a796bbeca92dcb107062b45b682
Le Trong Dat <datlt2306@gmail.com>
2022-03-12 15:25:44 +0700

    feat: create, list, read controller

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import productRoute from './routes/product';
 import morgan from 'morgan';
+import mongoose from 'mongoose';
 
 const app = express();
 // middleware
@@ -13,8 +14,12 @@ app.use(express.json())
 // routes
 app.use("/api", productRoute)
 
+// connection db
+mongoose.connect("mongodb://localhost:27017/we16310")
+    .then(() => console.log("Ket noi DB thanh cong"))
+    .catch(error => console.log(error))
 // connect
-const PORT = 3001;
+const PORT = 8000;
 app.listen(PORT, () => {
     console.log("Server của bạn đang chạy cổng ", PORT);
 });
--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,19 +1,38 @@
-// fake data
-const data = [
-    {id: 1, name: "Product A"}, 
-    {id: 2, name: "Product B"}
-];
+import mongoose from "mongoose";
+const Product = mongoose.model('Product', { name: String });
 
+export const create = async (req, res) => {
+    try {
+        const product = await new Product(req.body).save();
+        res.json(product);
+    } catch (error) {
+        res.status(400).json({
+            error: "Không thêm được sản phẩm"
+        })
+    }
+}
+export const list = async (req, res) => {
+    try {
+        const products = await Product.find({}).exec();
+        res.json(products);
+    } catch (error) {
+        res.status(400).json({
+            error: "Không có sản phẩm"
+        })
+    }
+}
 
-export const list = (req, res) => {
-    res.json(data);
-}
-export const create = (req, res) => {
-    data.push(req.body);
-    res.json(data);
-}
-export const get = (req, res) => {
-    res.json(data.find(item => item.id == req.params.id));
+export const get = async (req, res) => {
+    console.log(req.params.id)
+    try {
+        const product = await Product.findOne({_id: req.params.id}).exec();
+        res.json(product);
+    } catch (error) {
+        res.status(400).json({
+            error: "Không có sản phẩm"
+        })
+    }
+    // res.json(data.find(item => item.id == req.params.id));
 }
 export const remove = (req, res) => {
     res.json(data.filter(item => item.id != req.params.id));

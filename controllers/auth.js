cec5f416bbb827185771f995a322f3f26fe7f6f1
Le Trong Dat <datlt2306@gmail.com>
2022-03-26 10:20:39 +0700

    feat: verify token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,6 @@
 import User from "../models/user";
+import jwt from 'jsonwebtoken';
+
 
 export const signup = async (req, res) => {
     const { email, name, password} = req.body
@@ -36,7 +38,10 @@ export const signin = async (req, res) => {
             })
         }
 
+        const token = jwt.sign({_id: user._id }, "123456", { expiresIn: 60 * 60})
+
         res.json({
+            token,
             user: {
                 _id: user._id,
                 email: user.email,

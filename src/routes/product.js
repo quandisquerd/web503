319af05663d3ebd0b9ed650ce027599c48710b4d
Le Trong Dat <datlt2306@gmail.com>
2022-06-29 15:33:30 +0700

    update

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 const router = express.Router();
+
 const data = [
     { id: 1, name: "product A" },
     { id: 2, name: "product B" },
@@ -10,16 +11,52 @@ router.get("/products", (req, res) => {
     res.json(data);
 });
 router.get("/products/:id", (req, res) => {
-    console.log('thong tin params', req.params)
+    try {
+        const id = req.params.id;
+        const product = data.find(item => item.id == id);
+        res.json(product);    
+    } catch (error) {
+        res.status(400).json({
+            error: 'Không tìm thấy sản phẩm'
+        })
+    }
+    
 });
 router.post("/products", (req, res) => {
-    console.log('thong tin body', req.body)
+    try {
+        const product = req.body; 
+        data.push({id: data.length + 1, ...product})
+        res.json(data);
+    } catch (error) {
+        res.status(400).json({
+            error: 'Không thêm được sản phẩm'
+        })
+    }
 });
-router.put("/products", (req, res) => {
-    console.log('thong tin body', req.body)
+router.delete("/products/:id", (req, res) => {
+    try {
+        const id = req.params.id; 
+        const products = data.filter(item => item.id !== +id);
+        res.json(products);
+    } catch (error) {
+        res.status(400).json({
+            error: 'Không thêm được sản phẩm'
+        })
+    }
 });
-router.delete("/products", (req, res) => {
-    console.log('thong tin body', req.body)
+router.patch("/products/:id", (req, res) => {
+    try {
+        const id = req.params.id; 
+        const newProduct = {id, ...req.body};
+        const products = data.map(item => item.id == id ? newProduct : item);
+        res.json(products);
+    } catch (error) {
+        res.status(400).json({
+            error: 'Không thêm được sản phẩm'
+        })
+    }
 });
 
+
 export default router;
+

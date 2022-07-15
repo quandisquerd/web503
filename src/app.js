45731439b7943a7e35addf85261928480143462c
Le Trong Dat <datlt2306@gmail.com>
2022-07-15 15:02:08 +0700

    feat: Signup

--- src/app.js
+++ src/app.js
@@ -1,28 +1,29 @@
 // const express = require("express");
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import mongoose from 'mongoose';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
 
-
-import routerProduct from './routes/product';
-import routerCategory from './routes/category';
+import routerAuth from "./routes/auth";
+import routerProduct from "./routes/product";
+import routerCategory from "./routes/category";
 
 const app = express();
 
 // middleware
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-app.use("/api",routerCategory);
-app.use("/api",routerProduct);
+
+app.use("/api", routerAuth);
+app.use("/api", routerCategory);
+app.use("/api", routerProduct);
 
 // connect db
 
-mongoose.connect('mongodb://localhost:27017/we17103', () => {
-    console.log('successfully')
+mongoose.connect("mongodb://localhost:27017/we17103", () => {
+    console.log("successfully");
 });
 
 app.listen(process.env.PORT, () => {
-    console.log('Kết nối thành công, cổng ' + process.env.PORT)
-})
-
+    console.log("Kết nối thành công, cổng " + process.env.PORT);
+});

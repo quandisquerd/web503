e0747d1e6818ab41d6e912a9d733f4a83c5f10b8
Le Trong Dat <datlt2306@gmail.com>
2023-03-06 15:57:13 +0700

    Buổi 1

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
+// Cài đặt nodejs, default đã có rất package hỗ trợ
+
+// cú pháp ES5
 const http = require("http");
 
-const server = http.createServer(function (req, res) {
+const server = http.createServer((req, res) => {
     const products = [
-        { id: 1, name: "Product 1" },
-        { id: 2, name: "Product 2" },
+        { name: "Iphone 12", price: 2000 },
+        { name: "Iphone 11000", price: 1000 },
     ];
-
+    products.push({ name: "Iphone 10", price: 500 });
     if (req.url == "/") {
         res.setHeader("Content-Type", "text/html");
-        res.end(`<form action="/products" method="post">
-            <input type="text" placeholder="Product Name" name="productName" />
-            <button>Submit</button>
-        </form>`);
+        res.end(`<html>
+                    <body>
+                        <form action="/products" method="post">
+                            <input type="text" name="name" />
+                            <button>Submit</button>
+                        </form>
+                    </body>
+                </html>`);
     }
-
-    if (req.url == "/products") {
+    if (req.url == "/products" && req.method == "POST") {
         res.setHeader("Content-Type", "application/json");
-        products.push({ id: 3, name: "Product 3" });
         res.end(JSON.stringify(products));
     }
 });
-
-server.listen(8080, function () {
-    console.log("Server is running port 8080");
+server.listen(8080, () => {
+    console.log("Server is running on port 8080");
 });
+
 // npm init -y
-// npm install nodemon -D
+// npm i nodemon -D
 // "start": "nodemon app.js"

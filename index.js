56eda4a789ec0140d990915af1af22234068d1d2
Le Trong Dat <datlt2306@gmail.com>
2023-03-06 11:07:43 +0700

    Buổi 1 : req, res

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import http from "http";
 
 const server = http.createServer((req, res) => {
-    if (req.url == "/about") {
+    const name = "Dat";
+    if (req.url == "/") {
+        res.setHeader("Content-Type", "text/html");
+        res.end(`<form action="/about" method="post">
+                <input type="text" name="name"/>
+                <button>Submit</button>
+            </form>
+        `);
+    }
+    if (req.url == "/about" && req.method == "POST") {
         res.end(`<h1>hello ${name}</h1>`);
     }
 });
@@ -12,3 +21,9 @@ server.listen(8080, () => {
 // npm init -y
 // npm i -D nodemon
 // nodemon index.js
+// http://github.com/datlt2306/web503
+// polytuts.vercel.app
+// horbt4w
+
+//
+// /products?_sort=price&_order=desc

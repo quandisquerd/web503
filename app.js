f11e53e08c5f6f064096869148970df58cb23d69
Le Trong Dat <datlt2306@gmail.com>
2023-03-07 10:38:04 +0700

    init

--- app.js
+++ app.js
@@ -1,34 +1,19 @@
-// Cài đặt nodejs, default đã có rất package hỗ trợ
-
-// cú pháp ES5
 const http = require("http");
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(function (req, res) {
     const products = [
-        { name: "Iphone 12", price: 2000 },
-        { name: "Iphone 11000", price: 1000 },
+        { id: 1, name: "Product A" },
+        { id: 2, name: "Product B" },
     ];
-    products.push({ name: "Iphone 10", price: 500 });
     if (req.url == "/") {
         res.setHeader("Content-Type", "text/html");
-        res.end(`<html>
-                    <body>
-                        <form action="/products" method="post">
-                            <input type="text" name="name" />
-                            <button>Submit</button>
-                        </form>
-                    </body>
-                </html>`);
+        res.end(`<h1>Welcome to our home page</h1>`);
     }
-    if (req.url == "/products" && req.method == "POST") {
+    if (req.url === "/api/products") {
         res.setHeader("Content-Type", "application/json");
         res.end(JSON.stringify(products));
     }
 });
-server.listen(8080, () => {
-    console.log("Server is running on port 8080");
+server.listen(8080, function () {
+    console.log("Server running on port 8080");
 });
-
-// npm init -y
-// npm i nodemon -D
-// "start": "nodemon app.js"

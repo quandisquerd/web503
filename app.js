c1fe72cb04ff9e8267bb000f4a6568531d7de291
Le Trong Dat <datlt2306@gmail.com>
2023-03-08 15:35:17 +0700

    Buổi 2

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+const products = [
+    { id: 1, name: "Iphone 12", price: 2000 },
+    { id: 2, name: "Iphone 11000", price: 1000 },
+];
+
 // Cài đặt nodejs, default đã có rất package hỗ trợ
 
 // cú pháp ES5
 // const http = require("http");
 
 // const server = http.createServer((req, res) => {
-//     const products = [
-//         { name: "Iphone 12", price: 2000 },
-//         { name: "Iphone 11000", price: 1000 },
-//     ];
+
 //     products.push({ name: "Iphone 10", price: 500 });
 //     // routing
 //     if (req.url == "/") {
@@ -30,6 +32,7 @@
 //             const bodyParse = Buffer.concat(body).toString();
 //             console.log(bodyParse);
 //         });
+//         res.end(JSON.stringify(products));
 //     }
 // });
 // server.listen(8080, () => {
@@ -43,23 +46,50 @@
 import express from "express";
 
 const app = express();
-// chạy trước khi nhận dữ liệu
-app.use(express.urlencoded({ extended: true }));
-app.get("/", (req, res) => {
-    res.send(`
-    <html>
-    <body>
-        <form action="/products" method="post">
-            <input type="text" name="name" />
-            <button>Submit</button>
-        </form>
-    </body>
-    </html>
-    `);
-});
 
-app.post("/products", function (req, res) {
-    console.log(req.body);
+app.use(express.json());
+
+// Trả về danh sách
+app.get("/products", (req, res) => {
+    res.json(products);
+});
+// Trả về một sản phẩm
+app.get("/products/:id", (req, res) => {
+    const id = req.params.id;
+    const product = products.find((product) => product.id == id);
+    res.json(product);
+});
+// Thêm sản phẩm
+app.post("/products", (req, res) => {
+    // Lấy dữ liệu từ client body gửi lên
+    const product = req.body;
+    // push vào mảng products
+    products.push(product);
+    // trả về client dạng json
+    res.json({
+        message: "Thêm sản phẩm thành công",
+        products,
+    });
+});
+// Xóa sản phẩm
+app.delete("/products/:id", (req, res) => {
+    const id = req.params.id;
+    const newProducts = products.filter((item) => item.id != id);
+    res.status(200).json({
+        message: "Xóa sản phẩm thành công",
+        products: newProducts,
+    });
+});
+// Cập nhật sản phẩm
+
+app.patch("/products/:id", (req, res) => {
+    const id = req.params.id;
+    const body = req.body;
+    const newProducts = products.map((item) => (item.id == id ? body : item));
+    res.status(200).json({
+        message: "Cập nhật sản phẩm thành công",
+        products: newProducts,
+    });
 });
 
 app.listen(8080, () => {

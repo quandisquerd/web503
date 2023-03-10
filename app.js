e87e64fa9bf571d446c72a05c6818e2610975cd3
Le Trong Dat <datlt2306@gmail.com>
2023-03-10 14:54:47 +0700

    Buổi 3: update

--- app.js
+++ app.js
@@ -1,52 +1,46 @@
 import express from "express";
+import axios from "axios";
 
 const app = express();
 
 app.use(express.json());
 
 // Trả về danh sách
-app.get("/products", (req, res) => {
-    res.json(products);
+app.get("/api/products", async (req, res) => {
+    const { data } = await axios.get("http://localhost:3002/products");
+    return res.json(data);
 });
 // Trả về một sản phẩm
-app.get("/products/:id", (req, res) => {
+app.get("/api/products/:id", async (req, res) => {
     const id = req.params.id;
-    const product = products.find((product) => product.id == id);
-    res.json(product);
+    const { data } = await axios.get(`http://localhost:3002/products/${id}`);
+    return res.json(data);
 });
 // Thêm sản phẩm
-app.post("/products", (req, res) => {
-    // Lấy dữ liệu từ client body gửi lên
-    const product = req.body;
-    // push vào mảng products
-    products.push(product);
-    // trả về client dạng json
-    res.json({
+app.post("/api/products", async (req, res) => {
+    const body = req.body;
+    const { data } = await axios.post(`http://localhost:3002/products`, body);
+    return res.json({
         message: "Thêm sản phẩm thành công",
-        products,
+        data,
     });
 });
 // Xóa sản phẩm
-app.delete("/products/:id", (req, res) => {
-    const id = req.params.id;
-    const newProducts = products.filter((item) => item.id != id);
-    res.status(200).json({
+app.delete("/api/products/:id", async (req, res) => {
+    await axios.delete(`http://localhost:3002/products/${req.params.id}`);
+    return res.json({
         message: "Xóa sản phẩm thành công",
-        products: newProducts,
     });
 });
 // Cập nhật sản phẩm
-
-app.patch("/products/:id", (req, res) => {
-    const id = req.params.id;
-    const body = req.body;
-    const newProducts = products.map((item) => (item.id == id ? body : item));
-    res.status(200).json({
+app.patch("/api/products/:id", async (req, res) => {
+    const { data } = await axios.patch(`http://localhost:3002/products/${req.params.id}`, req.body);
+    res.json({
         message: "Cập nhật sản phẩm thành công",
-        products: newProducts,
+        data,
     });
 });
 
-app.listen(8080, () => {
-    console.log("Server is running on port 8080");
+app.listen(8081, () => {
+    console.log("Server is running on port 8081");
 });

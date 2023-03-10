36acd551ff9a709cd3080beef15d16a80c8bbfe0
Le Trong Dat <datlt2306@gmail.com>
2023-03-10 08:17:44 +0700

    buổi 3: json-server

--- app.js
+++ app.js
@@ -7,15 +7,14 @@ const app = express();
 app.use(express.json());
 
 app.get("/api/products", async (req, res) => {
-    const { data } = await axios.get(`https://63f5d86059c944921f67a58c.mockapi.io/products`);
+    const { data } = await axios.get(`http://localhost:3001/products`);
     res.json(data);
 });
 app.get("/api/products/:id", async (req, res) => {
-    const { data } = await axios.get(
-        `https://63f5d86059c944921f67a58c.mockapi.io/products/${req.params.id}`
-    );
+    const { data } = await axios.get(`http://localhost:3001/products/${req.params.id}`);
     res.json(data);
 });
+
 app.post("/api/products", (req, res) => {
     console.log(req.body);
 });

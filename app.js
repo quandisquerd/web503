269240ec6159da3bd0c62db56c0b610d07105824
Le Trong Dat <datlt2306@gmail.com>
2023-03-10 08:11:53 +0700

    Buổi 3: axios

--- app.js
+++ app.js
@@ -1,63 +1,20 @@
-// import http from "http";
-
-// const server = http.createServer((req, res) => {
-//     const body = [];
-//     const products = [
-//         { id: 1, name: "Product A" },
-//         { id: 2, name: "Product B" },
-//         { id: 3, name: "Product C" },
-//     ];
-//     if (req.url === "/") {
-//         res.setHeader("Content-Type", "text/html");
-//         res.end(`
-//             <html>
-//                 <body>
-//                     <form action="/api/products" method="post">
-//                         <input type="text" name="name" />
-//                         <button>Submit</button>
-//                     </form>
-//                 </body>
-//             </html>
-//         `);
-//     }
-//     if (req.url == "/api/products" && req.method == "POST") {
-//         req.on("data", function (chunk) {
-//             body.push(chunk);
-//         });
-//         req.on("end", () => {
-//             // name=product A
-//             const bodyParse = Buffer.concat(body).toString();
-//             // Product A
-//             const data = bodyParse.split("=")[1];
-//             // them vao products
-//             products.push(data);
-//             console.log(products);
-//         });
-//     }
-// });
-
-// server.listen(8080, () => {
-//     console.log("Server is running on port 8080");
-// });
-
 import express from "express";
+import axios from "axios";
 
 // khởi tạo
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send(`
-        <html>
-            <body>
-                <form action="/api/products" method="post">
-                    <input type="text" name="name" />
-                    <button>Submit</button>
-                </form>
-            </body>
-        </html>
-    `);
+app.get("/api/products", async (req, res) => {
+    const { data } = await axios.get(`https://63f5d86059c944921f67a58c.mockapi.io/products`);
+    res.json(data);
+});
+app.get("/api/products/:id", async (req, res) => {
+    const { data } = await axios.get(
+        `https://63f5d86059c944921f67a58c.mockapi.io/products/${req.params.id}`
+    );
+    res.json(data);
 });
 app.post("/api/products", (req, res) => {
     console.log(req.body);

e21cb836286795c471f02cb76d49faa2e774af1e
Le Trong Dat <datlt2306@gmail.com>
2023-03-10 12:50:47 +0700

    Buổi 3: refactor

--- app.js
+++ app.js
@@ -1,90 +1,36 @@
-const products = [
-    { id: 1, name: "Product 1" },
-    { id: 2, name: "Product 2" },
-];
-
-// const http = require("http");
-
-// const server = http.createServer(function (req, res) {
-
-//     // routing - router
-//     if (req.url == "/") {
-//         res.setHeader("Content-Type", "text/html");
-//         res.end(`<form action="/products" method="post">
-//             <input type="text" placeholder="Product Name" name="productName" />
-//             <button>Submit</button>
-//         </form>`);
-//     }
-
-//     if (req.url == "/products" && req.method == "POST") {
-//         const body = [];
-//         req.on("data", function (chunk) {
-//             body.push(chunk);
-//         });
-//         req.on("end", function () {
-//             const bodyParse = Buffer.concat(body).toString();
-//             console.log(bodyParse.split("="));
-//             const user = {
-//                 name: bodyParse.split("=")[1],
-//             };
-//             res.end(JSON.stringify(user));
-//         });
-//     }
-// });
-
-// server.listen(8080, function () {
-//     console.log("Server is running port 8080");
-// });
-// npm init -y
-// npm install nodemon -D
-// "start": "nodemon app.js"
-// 3 loại lỗi:
-// 1. SyntaxError: Unexpected token < in JSON at position 0
-// 2. logic error
-// 3. runtime error
-
 import express from "express";
+import axios from "axios";
 
 const app = express();
 
 app.use(express.json());
 
 // Trả về danh sách
-app.get("/products", function (req, res) {
-    res.json(products);
+app.get("/api/products", async function (req, res) {
+    const { data } = await axios.get("http://localhost:3002/products");
+    res.json(data);
 });
 // Trả về 1 phần tử
-app.get("/products/:id", function (req, res) {
-    const id = req.params.id;
-    const product = products.find((product) => product.id == id);
-    res.json({
-        message: "Product found",
-        product,
-    });
+app.get("/api/products/:id", async function (req, res) {
+    // Chờ đợi kết quả trả về từ JSON server
+    const { data } = await axios.get("http//localhost:3002/products/" + req.params.id);
+    // Trả về kết quả
+    res.json(data);
 });
-app.post("/products", (req, res) => {
+app.post("/api/products", async (req, res) => {
     const body = req.body;
-    products.push(body);
+    const { data } = await axios.post("http://localhost:3002/products", body);
+    res.json(data);
+});
+app.delete("/api/products/:id", async (req, res) => {
+    await axios.delete("http://localhost:3002/products/" + req.params.id);
+    res.json({ message: "Xóa thành công" });
+});
+app.put("/api/products/:id", async (req, res) => {
+    const { data } = await axios.put("http://localhost:3002/products/" + req.params.id, req.body);
     res.json({
-        message: "Product created",
-        products,
-    });
-});
-app.delete("/products/:id", (req, res) => {
-    const id = req.params.id;
-    const newProducts = products.filter((product) => product.id != id);
-    res.status(200).json({
-        message: "Product deleted",
-        products: newProducts,
-    });
-});
-app.put("/products/:id", (req, res) => {
-    const id = req.params.id;
-    const body = req.body;
-    const newProducts = products.map((product) => (product.id == id ? body : product));
-    res.status(200).json({
-        message: "Product updated",
-        products: newProducts,
+        message: "Cập nhật thành công",
+        data,
     });
 });
 

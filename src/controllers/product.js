bcb712da450b5ff10a6e3440eb4a6540e6abbfa8
Le Trong Dat <datlt2306@gmail.com>
2023-03-11 10:22:22 +0700

    Buổi 3: updated

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -3,34 +3,80 @@ import axios from "axios";
 const API_URI = "http://localhost:3002/products";
 
 export const getAll = async (req, res) => {
-    const { data: products } = await axios.get(`${API_URI}`);
-    res.json(products);
+    try {
+        const { data: products } = await axios.get(`${API_URI}`);
+        if (products.length === 0) {
+            return res.json({
+                message: "Không có sản phẩm nào",
+            });
+        }
+        return res.json(products);
+    } catch (error) {
+        return res.status(400).json({
+            message: error,
+        });
+    }
 };
 export const get = async function (req, res) {
-    const { data: product } = await axios.get(`${API_URI}/${req.params.id}`);
-    res.json({
-        message: "Detail product",
-        data: product,
-    });
+    try {
+        const { data: product } = await axios.get(`${API_URI}/${req.params.id}`);
+        if (!product) {
+            return res.json({
+                message: "Không có sản phẩm nào",
+            });
+        }
+        return res.json(product);
+    } catch (error) {
+        return res.status(400).json({
+            message: error,
+        });
+    }
 };
 export const create = async function (req, res) {
-    const { data: product } = await axios.post(`${API_URI}`, req.body);
-    res.json({
-        message: "Thêm sản phẩm thành công",
-        data: product,
-    });
+    try {
+        const { data: product } = await axios.post(`${API_URI}`, req.body);
+        if (!product) {
+            return res.json({
+                message: "Không thêm sản phẩm",
+            });
+        }
+        return res.json({
+            message: "Thêm sản phẩm thành công",
+            data: product,
+        });
+    } catch (error) {
+        return res.status(400).json({
+            message: error,
+        });
+    }
 };
 export const update = async function (req, res) {
-    const { data: product } = await axios.patch(`${API_URI}/${req.params.id}`, req.body);
-    res.json({
-        message: "Cập nhật sản phẩm thành công",
-        data: product,
-    });
+    try {
+        const { data: product } = await axios.patch(`${API_URI}/${req.params.id}`, req.body);
+        if (!product) {
+            return res.json({
+                message: "Cập nhật sản phẩm không thành công",
+            });
+        }
+        return res.json({
+            message: "Cập nhật sản phẩm thành công",
+            data: product,
+        });
+    } catch (error) {
+        return res.status(400).json({
+            message: error,
+        });
+    }
 };
 export const remove = async function (req, res) {
-    const { data: product } = await axios.delete(`${API_URI}/${req.params.id}`);
-    res.json({
-        message: "Xóa sản phẩm thành công",
-        data: product,
-    });
+    try {
+        await axios.delete(`${API_URI}/${req.params.id}`);
+        res.json({
+            message: "Xóa sản phẩm thành công",
+        });
+    } catch (error) {
+        return res.status(400).json({
+            message: error,
+        });
+    }
 };

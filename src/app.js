ae202d85f40275702ab47a8a50e54a6d79ccf334
Le Trong Dat <datlt2306@gmail.com>
2023-03-13 14:37:58 +0700

    Buổi 4: vite + nodejs

--- src/app.js
+++ src/app.js
@@ -7,6 +7,10 @@ app.use(express.json());
 
 app.use("/api", productRouter);
 
-app.listen(8081, () => {
-    console.log("Server is running on port 8081");
-});
+export const viteNodeApp = app;
+
+// B1: npm i vite vite-plugin-node -D
+// B2: tạo file vite.config.js ở root -> copy code của thầy
+// B3: chỉnh sửa file package.json -> copy code của thầy
+// B4: chỉnh sửa file app.js -> export const viteNodeApp = app; ở cuối file
+// B5: npm run dev
--- src/routes/product.js
+++ src/routes/product.js
@@ -2,6 +2,7 @@ import express from "express";
 import { create, get, getAll, remove, update } from "../controllers/product";
 
 const router = express.Router();
+
 router.get("/products", getAll);
 router.get("/products/:id", get);
 router.post("/products", create);

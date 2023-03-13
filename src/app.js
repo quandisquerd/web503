423c8ef967ba85a60ae36959fa63fc467089d070
Le Trong Dat <datlt2306@gmail.com>
2023-03-13 12:12:11 +0700

    update

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ import productRouter from "./routes/product";
 
 const app = express();
 
-app.use("/api", productRouter);
-
+// middleware
 app.use(express.json());
 
+// router
+app.use("/api", productRouter);
+// server
 app.listen(8080, () => {
     console.log("Server is running on port 8080");
 });

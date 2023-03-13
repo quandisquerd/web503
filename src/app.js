fe33b7b3226400ae8bc678a294b8a8c8f2cd31b3
Le Trong Dat <datlt2306@gmail.com>
2023-03-13 12:18:52 +0700

    Buổi 4: setup vite chạy nodejs

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import productRouter from "./routes/product";
+
+const app = express();
+
+// middleware
+app.use(express.json());
+
+// router
+app.use("/api", productRouter);
+
+app.listen(8080, function () {
+    console.log("Server is running port 8080");
+});
+
+export const viteNodeApp = app;
+
+// npm i vite vite-plugin-node -D
+// Tạo file vite.config.js ở root -> clone github của thầy
+// truy cập file app.js thêm cuối file -> export const viteNodeApp = app;
+//

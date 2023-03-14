03bd4fbcbca74e70a66be97615366c9a27611d0d
Le Trong Dat <datlt2306@gmail.com>
2023-03-14 12:28:58 +0700

    Buổi 4: setup vitejs

--- src/app.js
+++ src/app.js
@@ -5,16 +5,14 @@ import productRouter from "./routes/product";
 dotenv.config();
 const app = express();
 
-app.use(productRouter);
 // đăng ký middleware" giải mã dữ liệu json
 app.use(express.json());
+
+// router
+app.use(productRouter);
 
 app.listen(process.env.PORT, function () {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
 
-// try/catch
-// npm i dotenv --save
-// npm i concurrently -g
-// routes -> sử dụng express.Router
-// controllers
+export const viteNodeApp = app;

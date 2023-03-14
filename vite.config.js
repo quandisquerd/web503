ebd38e555e59ecd6757faa54c0e9f026bf40393c
Le Trong Dat <datlt2306@gmail.com>
2023-03-14 12:29:59 +0700

    Buổi 4: update .env

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from "vite";
 import { VitePluginNode } from "vite-plugin-node";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default defineConfig({
     // ...vite configures
     server: {
         // vite server configs, for details see [vite doc](https://vitejs.dev/config/#server-host)
-        port: 8080,
+        port: process.env.PORT,
     },
     plugins: [
         ...VitePluginNode({

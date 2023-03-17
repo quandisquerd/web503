85f1e099716797208e7cc3462b22a11a90d1c71f
Le Trong Dat <datlt2306@gmail.com>
2023-03-17 09:17:58 +0700

    Buổi 6: update

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -51,10 +51,14 @@ export const create = async (req, res) => {
 };
 export const update = async (req, res) => {
     try {
-        const { data } = await axios.put(
-            `http://localhost:3000/products/${req.params.id}`,
-            req.body
-        );
+        // const { data } = await axios.put(
+        //     `http://localhost:3000/products/${req.params.id}`,
+        //     req.body
+        // );
+
+        const data = await Product.findOneAndUpdate({ _id: req.params.id }, req.body, {
+            new: true,
+        });
         if (data.length === 0) {
             return res.status(200).json({
                 message: "Cập nhật sản phẩm không thành công",
@@ -69,9 +73,10 @@ export const update = async (req, res) => {
 };
 export const remove = async (req, res) => {
     try {
-        await axios.delete(`http://localhost:3000/products/${req.params.id}`);
+        const data = await Product.findOneAndDelete({ _id: req.params.id });
         return res.json({
             message: "Xóa sản phẩm thành công",
+            data,
         });
     } catch (error) {
         return res.status(404).json({

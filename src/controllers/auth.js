fcfa76f629734d88a729a64b0e2eb36597f3766e
Le Trong Dat <datlt2306@gmail.com>
2023-03-23 10:58:36 +0700

    Buổi 8 : refactor signup

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,9 +1,19 @@
 import User from "../models/user";
+import { signupSchema } from "../schemas/auth";
+import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
     try {
         const { name, email, password, confirmPassword } = req.body;
 
+        const { error } = signupSchema.validate(req.body, { abortEarly: false });
+
+        if (error) {
+            const errors = error.details.map((err) => err.message);
+            return res.status(400).json({
+                message: errors,
+            });
+        }
         const userExists = await User.findOne({ email });
         if (userExists) {
             return res.status(400).json({
@@ -11,8 +21,14 @@ export const signup = async (req, res) => {
             });
         }
 
-        const user = await User.create(req.body);
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword,
+        });
 
+        user.password = undefined;
         return res.status(201).json({
             message: "User created successfully",
             user,

44c4c5c2397f26c1676f4c6cdb61e0185c32ac04
Le Trong Dat <datlt2306@gmail.com>
2023-03-24 13:48:18 +0700

    jwt

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,7 @@
 import User from "../models/user";
 import { signupSchema } from "../schemas/auth";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res) => {
     try {
@@ -18,9 +20,20 @@ export const signup = async (req, res) => {
                 message: "Email đã tồn tại",
             });
         }
-        const user = await User.create(req.body);
+
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+        const user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword,
+        });
+
+        const accessToken = jwt.sign({ _id: user._id }, "banThayDat", { expiresIn: "1d" });
+
         return res.status(201).json({
             message: "Đăng ký tk thành công",
+            accessToken,
             user,
         });
     } catch (error) {

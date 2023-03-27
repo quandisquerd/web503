c8b79fa91114cd7011c95bf8a441e05b78e8a712
Le Trong Dat <datlt2306@gmail.com>
2023-03-27 12:27:21 +0700

    Signin

--- src/schemas/auth.js
+++ src/schemas/auth.js
@@ -20,3 +20,16 @@ export const signupSchema = joi.object({
         "any.only": "Xác nhận mật khẩu không khớp",
     }),
 });
+
+export const signinSchema = joi.object({
+    email: joi.string().email().required().messages({
+        "string.empty": "Email không được để trống",
+        "any.required": "Trường email là bắt buộc",
+        "string.email": "Email không đúng định dạng",
+    }),
+    password: joi.string().required().min(6).messages({
+        "string.empty": "Mật khẩu không được để trống",
+        "any.required": "Trường mật khẩu là bắt buộc",
+        "string.min": "Mật khẩu phải có ít nhất {#limit} ký tự",
+    }),
+});

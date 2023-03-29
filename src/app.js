197f84596d0e3dca6f523598ed60589615c055fa
Le Trong Dat <datlt2306@gmail.com>
2023-03-29 12:13:36 +0700

    update

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 import express from "express";
 import productRouter from "./routes/product";
 import authRouter from "./routes/auth";
+import cors from "cors";
 
 import mongoose from "mongoose";
 const app = express();
 
 // middleware
 app.use(express.json());
-
+app.use(cors());
 // router
 app.use("/api", productRouter);
 app.use("/api", authRouter);

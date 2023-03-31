e4a1958abb8062628350d6a58a8e55318e7bd076
Le Trong Dat <datlt2306@gmail.com>
2023-03-31 13:08:40 +0700

    refactor

--- src/middlewares/checkPermission.js
+++ src/middlewares/checkPermission.js
@@ -3,35 +3,21 @@ import User from "../models/user";
 
 export const checkPermission = async (req, res, next) => {
     try {
-        if (!req.headers.authorization) {
-            return res.status(400).json({
-                message: "Bạn chưa đăng nhập",
-            });
+        const token = req.headers.authorization?.split(" ")[1];
+        if (!token) {
+            throw new Error("Bạn chưa đăng nhập");
         }
-        const token = req.headers.authorization.split(" ")[1];
-
-        jwt.verify(token, "banThayDat", async (error, payload) => {
-            if (error) {
-                if (error.name == "TokenExpiredError") {
-                    return res.status(401).json({
-                        message: "Token hết hạn",
-                    });
-                }
-                if (error.name == "JsonWebTokenError") {
-                    return res.status(401).json({
-                        message: "Token không hợp lệ",
-                    });
-                }
-            }
-            const user = await User.findById(payload._id);
-            if (user.role !== "admin") {
-                return res.status(401).json({
-                    message: "Bạn không có quyền truy cập tài nguyên này",
-                });
-            }
-            next();
+        const decoded = jwt.verify(token, "banThayDat");
+        const user = await User.findById(decoded._id);
+        if (!user || user.role !== "admin") {
+            throw new Error("Bạn không có quyền truy cập tài nguyên này");
+        }
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            message: error.message || "Token không hợp lệ",
         });
-    } catch (error) {}
+    }
 };
 // B1: Kiểm tra trong header.authorization có token không? Nếu không có thì trả về lỗi
 // B2: Kiểm tra token có hợp lệ không? Nếu hợp lệ thì decode

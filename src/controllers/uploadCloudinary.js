d46b50f5cf97fa1abc9d1bfdb345507f6bfb01cf
Le Trong Dat <datlt2306@gmail.com>
2023-04-04 09:19:43 +0700

    refactor upload image

--- src/controllers/uploadCloudinary.js
+++
@@ -1,4 +0,0 @@
-export const uploadImage = async (req, res) => {
-    const urls = req.files.map((file) => file.path);
-    return res.json({ urls: urls });
-};
--- src/routes/uploadImage.js
+++ src/routes/uploadImage.js
@@ -1,15 +1,9 @@
 import express from "express";
-import { v2 as cloudinary } from "cloudinary";
-import { CloudinaryStorage } from "multer-storage-cloudinary";
 import multer from "multer";
-import { uploadImage } from "../controllers/uploadCloudinary";
-
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import { deleteImage, uploadImage } from "../controllers/upload";
+import cloudinary from "../config/cloudinaryConfig";
 const router = express.Router();
-cloudinary.config({
-    cloud_name: "ecommercer2021",
-    api_key: "626155946999554",
-    api_secret: "7VZ2gYWaR0ZWKGfd55uBPIjEnso",
-});
 
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
@@ -21,5 +15,7 @@ const storage = new CloudinaryStorage({
 
 const upload = multer({ storage: storage });
 
-router.post("/upload", upload.array("images", 10), uploadImage);
+router.post("/images/upload", upload.array("images", 10), uploadImage);
+router.delete("/images/:publicId", deleteImage);
+
 export default router;

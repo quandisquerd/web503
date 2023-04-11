71ef167c86848176a2162ce1c4b7f9d6367512ee
Le Trong Dat <datlt2306@gmail.com>
2023-04-12 00:22:43 +0700

    base

--- src/app.js
+++ src/app.js
@@ -1,23 +1,12 @@
 import express from "express";
-import dotenv from "dotenv";
-import productRouter from "./routes/product";
-import categoryRouter from "./routes/category";
-import authRouter from "./routes/auth";
 import mongoose from "mongoose";
 import cors from "cors";
 
-dotenv.config();
-
-// khởi tạo
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use("/api", productRouter);
-app.use("/api", categoryRouter);
-app.use("/api", authRouter);
-
-mongoose.connect(process.env.URI);
+mongoose.connect(`mongodb://localhost:27017/bai-thi`);
 
 export const viteNodeApp = app;
